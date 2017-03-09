790ce1db6bb43171e62c752779dd1e57350548de
lalnuo <lalli.nuorteva@relex.fi>
2017-03-10 01:00:01 +0200

    Really simple js importer

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 0.1.0 - First Release
+* Support importing any selected word from absolute path
---
+++ spec/atom-js-import-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomJsImportView from '../lib/atom-js-import-view';
+
+describe('AtomJsImportView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
